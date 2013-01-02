7549f176d629358eb1c3df1701c712c560ff62c2
Marius Mueller <mmuelle5@hsz-t.ch>
2013-01-02 22:52:42 +0100

    applikation beenden

--- src/ch/imbApp/cash_next_door/MainActivity.java
+++ src/ch/imbApp/cash_next_door/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends Activity {
 
 				Intent cameraActivity = new Intent(context, CameraActivity.class);
 				startActivity(cameraActivity);
+				finish();
 			}
 		});
 	}
