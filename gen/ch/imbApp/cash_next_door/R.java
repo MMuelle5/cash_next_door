d3c7316a73a8ffa4fb4b1fce3ced9ac1e772dc3c
Marius Mueller <mmuelle5@hsz-t.ch>
2013-01-03 22:57:53 +0100

    drawable gschicht

--- gen/ch/imbApp/cash_next_door/R.java
+++ gen/ch/imbApp/cash_next_door/R.java
@@ -11,22 +11,23 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int direction=0x7f020000;
-        public static final int ic_action_search=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int screen_home=0x7f020003;
+        public static final int bg_shape=0x7f020000;
+        public static final int direction=0x7f020001;
+        public static final int ic_action_search=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int screen_home=0x7f020004;
     }
     public static final class id {
         public static final int cameraButton=0x7f070002;
         public static final int camera_preview=0x7f070000;
-        public static final int cashOmat1=0x7f070007;
-        public static final int cashOmat2=0x7f070008;
-        public static final int cashOmat3=0x7f070009;
+        public static final int cashOmat1=0x7f070004;
+        public static final int cashOmat2=0x7f070006;
+        public static final int cashOmat3=0x7f070007;
+        public static final int cashOmat4=0x7f070008;
+        public static final int cashOmat5=0x7f070009;
         public static final int destroyMe=0x7f07000d;
         public static final int frameLayout2=0x7f070003;
-        public static final int imageView1=0x7f070004;
-        public static final int imageView2=0x7f070005;
-        public static final int imageView3=0x7f070006;
+        public static final int imageView1=0x7f070005;
         public static final int info=0x7f07000c;
         public static final int machineListInfos=0x7f07000a;
         public static final int preferedMachine=0x7f07000b;
